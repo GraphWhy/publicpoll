fdbb3681e030ba773dc771091fdd852425fdeaeb
AlexanderMcNulty <alexander.mcnulty92@gmail.com>
2018-12-25 00:47:38 -0500

    add comments to __init__ file

--- publicpoll/__init__.py
+++ publicpoll/__init__.py
@@ -1,6 +1,9 @@
 from pyramid.config import Configurator
 
-
+# Returns a router to act as a WSGI which is used to start a WSGI server, Waitress
+# **settings refers to a set of key/value pairs in development and production.ini
+# more information under "startup" in the pyramid narrative docs
+# https://docs.pylonsproject.org/projects/pyramid/en/latest/narr/startup.html#startup-chapter
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
