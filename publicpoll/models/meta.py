d36a7aaac88071dfa59a52e4c37cc048bcf1ca63
AlexanderMcNulty <alexander.mcnulty92@gmail.com>
2018-12-28 16:29:55 -0500

    add explanatory comments in meta file

--- publicpoll/models/meta.py
+++ publicpoll/models/meta.py
@@ -5,10 +5,15 @@ from sqlalchemy.schema import MetaData
 # providers will autogenerate vastly different names making migrations more
 # difficult. See: http://alembic.zzzcomputing.com/en/latest/naming.html
 NAMING_CONVENTION = {
+    # Index
     "ix": "ix_%(column_0_label)s",
+    # unique constraint
     "uq": "uq_%(table_name)s_%(column_0_name)s",
+    # Check Constraint
     "ck": "ck_%(table_name)s_%(constraint_name)s",
+    # Foreign Key 
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    # Primary Key Constraint
     "pk": "pk_%(table_name)s"
 }
 
